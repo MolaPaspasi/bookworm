9866b85452b2569218335515c863f0d85e4a283b
efe <xlargemoodx@gmail.com>
2025-10-11 20:06:09 +0300

    fixed

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
+import protectRoute from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
